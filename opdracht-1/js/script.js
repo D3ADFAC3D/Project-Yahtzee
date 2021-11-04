d77e235632d5d1f83f8b8bf9e1ec2d65f301a2a2
Daan Roering <0322418@student.rocvantwente.nl>
2021-11-04 13:50:54 +0100

    new js

--- opdracht-1/js/script.js
+++ opdracht-1/js/script.js
@@ -1,17 +1,4 @@
-getRandom = function () {
-    random_index = Math.floor(Math.random() * image_array.length);
-
-    selected_image = image_array[random_index];
-
-    document.getElementById('image_shower').src = `./pictures/${selected_image}`
+rollDice = function () {
+    let random =  Math.floor(Math.random() * 6) + 1;
+    return random;
 }
-
-image_array = [
-    'dice_1.jpg',
-    'dice_2.jpg',
-    'dice_3.jpg',
-    'dice_4.jpg',
-    'dice_5.jpg',
-    'dice_6.jpg'
-]
-
